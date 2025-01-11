f9b2ec5e611f309ed645bfadaea2d1813f6526e0
Casper Östman <74548230+C0ssu@users.noreply.github.com>
2025-01-11 15:06:37 +0200

    Added Damage Trigger Script To Debug

---
+++ Assets/Project/Debug/DamageTrigger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class DamageTrigger : MonoBehaviour
+{
+    [SerializeField] float damageAmount;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.TryGetComponent<Health>(out Health health))
+        {
+            health.Damage(damageAmount);
+        }
+    }
+}

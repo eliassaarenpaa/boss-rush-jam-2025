4b284fef1818c9ab3245d05ee7cbb575cba4d1df
Casper Östman <74548230+C0ssu@users.noreply.github.com>
2025-01-12 17:08:22 +0200

    Moved HealthBarSlider.cs to correct folder

---
+++ Assets/Project/Sandboxes/Cossu/CombatDemo/Scripts/HealthBarSlider.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Project.Sandboxes.Cossu.CombatDemo
+{
+    [RequireComponent(typeof(Slider))]
+    public class HealthBarSlider : MonoBehaviour
+    {
+        [SerializeField] Health trackedHealth;
+        [SerializeField] Slider slider;
+
+        private void Awake()
+        {
+            slider = GetComponent<Slider>();
+            slider.maxValue = trackedHealth.GetMaxHealth();
+            slider.value = trackedHealth.GetCurrentHealth();
+        }
+
+        private void OnEnable()
+        {
+            trackedHealth.onCurrentHealthChanged += UpdateValue;
+            trackedHealth.onMaxHealthChanged += UpdateMaxValue;
+        }
+
+        private void OnDisable()
+        {
+            trackedHealth.onCurrentHealthChanged -= UpdateValue;
+            trackedHealth.onMaxHealthChanged -= UpdateMaxValue;
+        }
+
+        private void UpdateValue(float newValue, Health.CurrentHealthChangeType type)
+        {
+            slider.value = newValue;
+        }
+
+        private void UpdateMaxValue(float newValue, Health.MaxHealthChangeType type)
+        {
+            slider.maxValue = newValue;
+        }
+    }
+}

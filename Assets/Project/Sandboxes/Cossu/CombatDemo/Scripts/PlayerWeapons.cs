73278ba6f953fc0772e05a3263d11bc47fc23bec
Casper Östman <lord.caspar@gmail.com>
2025-01-15 12:36:22 +0200

    Continued working on the combat system

--- Assets/Project/Sandboxes/Cossu/CombatDemo/Scripts/PlayerWeapons.cs
+++ Assets/Project/Sandboxes/Cossu/CombatDemo/Scripts/PlayerWeapons.cs
@@ -7,12 +7,28 @@ public class PlayerWeapons : MonoBehaviour
     [SerializeField] Transform projectileSpawn;
     [SerializeField] GameObject projectileBasePrefab;
 
+    private float lastShootTime = 0f;
     private void Update()
     {
         //Check input
         if (PlayerInput.Attack)
         {
-            //Attack code
+            if(currentTarot.tarotBaseData.fireRate + lastShootTime < Time.time) //Fire rate is ok
+            {
+                lastShootTime = Time.time;
+            }
+            else
+            {
+                Shoot();
+            }
         }
+    }
+
+    private void Shoot()
+    {
+        //Spawn projectileBasePrefab.
+        //Pass projectile data holder the data of the projectile
+        ProjectileDataHolder pDataHolder = Instantiate(projectileBasePrefab, projectileSpawn.position, projectileSpawn.rotation).GetComponent<ProjectileDataHolder>();
+        pDataHolder.tarotDataObject = currentTarot;
     }
 }

13869c1459e593f44ee24586f0e39cc54da30cae
Elias Saarenpää <elias.saarenpaa@gmail.com>
2025-01-16 21:50:15 +0200

    Added scriptable values and weapon ammo

---
+++ Assets/Project/Sandboxes/ScriptableValues/IntValue.cs
@@ -0,0 +1,13 @@
+﻿namespace Project.Sandboxes.ScriptableValues
+{
+    /// <summary>
+    ///     In case we need to add some specific functionality to all int values
+    /// </summary>
+    [UnityEngine.CreateAssetMenu(fileName = "IntValue", menuName = "Values/IntValue", order = 99999)]
+    public class IntValue : ScriptableValue<int>
+    {
+        public bool isZero => Value == 0;
+        public bool isGreaterThanZero => Value > 0;
+        public bool isLessThanZero => Value < 0;
+    }
+}
---
+++ Assets/Project/Sandboxes/ScriptableValues/ScriptableValue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Project.Sandboxes.ScriptableValues
+{
+    public class ScriptableValue<T> : ScriptableObject
+    {
+        private T _value;
+        public Action<T> OnValueChanged;
+
+        [ShowInInspector]
+        public T Value
+        {
+            get => _value;
+
+            set
+            {
+                if (value.Equals(_value))
+                {
+                    return;
+                }
+
+                _value = value;
+
+                OnValueChanged?.Invoke(_value);
+            }
+        }
+
+        public void SetValue(T value)
+        {
+            Value = value;
+        }
+
+        public void SetValue(ScriptableValue<T> value)
+        {
+            Value = value.Value;
+        }
+
+
+        /// <summary>
+        ///     Implicit conversion from ScriptableValue to T
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator T(ScriptableValue<T> value)
+        {
+            return value.Value;
+        }
+
+        /// <summary>
+        ///     Implicit conversion from T to ScriptableValue
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator ScriptableValue<T>(T value)
+        {
+            var instance = CreateInstance<ScriptableValue<T>>();
+            instance.Value = value;
+            return instance;
+        }
+    }
+}

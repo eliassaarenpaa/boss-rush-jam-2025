63fb85fe85635c41a1de48508d54b5acc8fae862
Elias Saarenpää <elias.saarenpaa@gmail.com>
2025-01-16 19:13:19 +0200

    Added simple shotgun shoot on mouse down logic

---
+++ Assets/Project/Sandboxes/Shotgun/ShotgunShoot.cs
@@ -0,0 +1,34 @@
+using Animancer;
+using UnityEngine;
+
+namespace Project.Sandboxes.Shotgun
+{
+    public class ShotgunShoot : MonoBehaviour
+    {
+        public AnimancerComponent anim;
+        public ClipTransition shotgun_shoot;
+        [SerializeField] private float firerate = 0.5f;
+
+        private float _timeSinceLastShot;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (Time.time - _timeSinceLastShot > firerate)
+                {
+                    _timeSinceLastShot = Time.time;
+                    OnShoot();
+                }
+            }
+        }
+
+        private void OnShoot()
+        {
+            anim.Stop(shotgun_shoot);
+            anim.Play(shotgun_shoot);
+
+            // TODO: Add shotgun projectile shoot logic here
+        }
+    }
+}

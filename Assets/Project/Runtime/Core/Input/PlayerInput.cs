14844859562e9d9dce1dedddf6057a1906c62d37
Elias Saarenpää <elias.saarenpaa@gmail.com>
2025-01-16 23:56:56 +0200

    Added new player input fields

--- Assets/Project/Runtime/Core/Input/PlayerInput.cs
+++ Assets/Project/Runtime/Core/Input/PlayerInput.cs
@@ -4,10 +4,13 @@ namespace Project.Runtime.Core.Input
 {
     public static class PlayerInput
     {
-        public static Vector2 MouseDelta => new Vector2(UnityEngine.Input.GetAxis("Mouse X"), UnityEngine.Input.GetAxis("Mouse Y")); 
+        public static Vector2 MouseDelta => new Vector2(UnityEngine.Input.GetAxis("Mouse X"), UnityEngine.Input.GetAxis("Mouse Y"));
         public static Vector3 Move => new Vector3(UnityEngine.Input.GetAxisRaw("Horizontal"), 0, UnityEngine.Input.GetAxisRaw("Vertical"));
+        public static bool IsHoldingMoveDirection => Move.magnitude > 0;
         public static bool Crouching => UnityEngine.Input.GetKey(KeyCode.LeftControl) || UnityEngine.Input.GetKey(KeyCode.C);
         public static bool Jump => UnityEngine.Input.GetKeyDown(KeyCode.Space);
+        public static bool JumpHeld => UnityEngine.Input.GetKey(KeyCode.Space);
         public static bool Attack => UnityEngine.Input.GetMouseButtonDown(0);
-    } 
+        public static bool Dash => UnityEngine.Input.GetKeyDown(KeyCode.LeftShift);
+    }
 }

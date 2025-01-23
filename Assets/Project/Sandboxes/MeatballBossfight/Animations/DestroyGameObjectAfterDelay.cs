1c31c2c72d7e5151d6ecd143b08ba7daf925cdf5
Elias Saarenpää <elias.saarenpaa@gmail.com>
2025-01-23 22:03:57 +0200

    Added meatball boss model

---
+++ Assets/Project/Sandboxes/MeatballBossfight/Animations/DestroyGameObjectAfterDelay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project.Sandboxes.MeatballBossfight.Animations
+{
+    public class DestroyGameObjectAfterDelay : MonoBehaviour
+    {
+        [SerializeField] private float delay = 0.0f;
+
+        private void OnEnable()
+        {
+            Destroy(gameObject, delay);
+        }
+    }
+}
